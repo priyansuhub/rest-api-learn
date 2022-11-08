1e75389edab8c15e02fee27d4dff181bb8cfa252
priyansuhub <muniapriyansu@gmail.com>
2022-11-08 20:37:57 +0530

    first commit

--- src/main/java/com/priyansuhub/prcode/repository/DepartmentRepository.java
+++ src/main/java/com/priyansuhub/prcode/repository/DepartmentRepository.java
@@ -3,7 +3,7 @@ package com.priyansuhub.prcode.repository;
 import com.priyansuhub.prcode.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//lala
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 }
